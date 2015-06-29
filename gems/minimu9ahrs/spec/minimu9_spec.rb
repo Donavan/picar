0ce0caaba1d1e19f451d9b09f1bfa16ef032d7c8
donavan <donavan.stanley@gmail.com>
2015-06-29 16:13:10 +0000

    Some basic tests around the imu API

--- gems/minimu9ahrs/spec/minimu9_spec.rb
+++ gems/minimu9ahrs/spec/minimu9_spec.rb
@@ -51,6 +51,53 @@ describe Minimu9ahrs::MinIMU9 do
     it 'can provide raw sensor data from the gryo' do
       expect(@minimu).to respond_to(:raw_gyro)
     end
+  end
+
+  describe 'raw data' do
+    before(:all) do
+      @minimu= clazz.new(test_i2c_device)
+    end
+
+    it 'returns an array of 3 floats for raw magnetometer data' do
+      raw_data = @minimu.raw_mag
+      expect(raw_data.class).to eq(Array)
+      expect(raw_data.count).to eq(3)
+      (0..2).each do |index|
+        expect(raw_data[index].class).to eq(Float), "Expected raw magnetometer data element #{index} to be a float got #{raw_data[index].class}."
+      end
+    end
+
+    it 'returns an array of 3 floats for raw accelerometer data' do
+      raw_data = @minimu.raw_acc
+      expect(raw_data.class).to eq(Array)
+      expect(raw_data.count).to eq(3)
+      (0..2).each do |index|
+        expect(raw_data[index].class).to eq(Float), "Expected raw accelerometer data element #{index} to be a float got #{raw_data[index].class}."
+      end
+    end
+
+    it 'returns an array of 3 floats for raw gryo data' do
+      raw_data = @minimu.raw_gyro
+      expect(raw_data.class).to eq(Array)
+      expect(raw_data.count).to eq(3)
+      (0..2).each do |index|
+        expect(raw_data[index].class).to eq(Float), "Expected raw gyro data element #{index} to be a float got #{raw_data[index].class}."
+      end
+    end
+
+    it 'returns an array of containing 3 arrays of 3 floats for raw data' do
+      raw_data = @minimu.raw_data
+      expect(raw_data.class).to eq(Array)
+      expect(raw_data.count).to eq(3)
+
+      raw_data.each_with_index  do |sensor_data, sensors_index|
+        expect(sensor_data.class).to eq(Array), "Expected raw data element #{sensors_index} to be an Array got #{sensor_data.class}."
+        expect(sensor_data.count).to eq(3), "Expected raw data element #{sensors_index} to have three elements got #{sensor_data.count}."
+        (0..2).each do |data_index|
+          expect(sensor_data[data_index].class).to eq(Float), "Expected raw sensor data element #{sensors_index} for senesor #{sensors_index} to be a float got #{sensor_data[data_index].class}."
+        end
+      end
+    end
 
   end
 
