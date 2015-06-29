ddc3a52d209ffecf9439e6ff1b5076126a4d770c
donavan <donavan.stanley@gmail.com>
2015-06-29 17:21:03 +0000

    Turn off a couple compile flags that aren't needed for C++

--- gems/minimu9ahrs/ext/minimu9ahrs/extconf.rb
+++ gems/minimu9ahrs/ext/minimu9ahrs/extconf.rb
@@ -2,4 +2,7 @@ require 'mkmf-rice'
 
 $CFLAGS = '-std=c++0x ' + $CFLAGS
 
+CONFIG['warnflags'].slice!(/ -Wdeclaration-after-statement/)
+CONFIG['warnflags'].slice!(/ -Wimplicit-function-declaration/)
+
 create_makefile('minimu9ahrs/minimu9ahrs')
